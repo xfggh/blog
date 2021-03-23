37b9c23cc3caed797c91154a99c2039c9845c5ec
xfggh <942590550@qq.com>
2021-03-23 10:12:40 +0800

    20210323-修改启动日志

--- src/main/java/com/xfggh/blog/BlogApplication.java
+++ src/main/java/com/xfggh/blog/BlogApplication.java
@@ -1,13 +1,23 @@
 package com.xfggh.blog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class BlogApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(BlogApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
+		SpringApplication app = new SpringApplication(BlogApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("Blog 启动成功");
+		LOG.info("地址：http://127.0.0.1:{}", env.getProperty("server.port"));
+
+		//		SpringApplication.run(BlogApplication.class, args);
 	}
 
 }
