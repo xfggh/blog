7d52fef4e22fcee5560551cae759452861a0cfe8
xfggh <942590550@qq.com>
2021-03-23 11:02:32 +0800

    20210323-添加自定义配置项

--- src/main/java/com/xfggh/blog/controller/HelloController.java
+++ src/main/java/com/xfggh/blog/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.xfggh.blog.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${test.hello:默认值}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello//" + testHello;
     }
 }
