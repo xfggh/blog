902af8a5746e0ca17284ccb220d393ec7022ce59
xfggh <942590550@qq.com>
2021-03-23 12:40:41 +0800

    20210323-添加热部署

--- src/main/java/com/xfggh/blog/controller/HelloController.java
+++ src/main/java/com/xfggh/blog/controller/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "hello//" + testHello;
+        return "hello world//" + testHello;
     }
 }
