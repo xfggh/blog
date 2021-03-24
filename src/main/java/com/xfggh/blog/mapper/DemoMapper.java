abf94b7f5586f1b3eb29139b5a9ced79f77a6521
xfggh <942590550@qq.com>
2021-03-24 14:37:02 +0800

    20210324-集成 mybatis-generator

---
+++ src/main/java/com/xfggh/blog/mapper/DemoMapper.java
@@ -0,0 +1,30 @@
+package com.xfggh.blog.mapper;
+
+import com.xfggh.blog.entity.Demo;
+import com.xfggh.blog.entity.DemoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DemoMapper {
+    long countByExample(DemoExample example);
+
+    int deleteByExample(DemoExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    List<Demo> selectByExample(DemoExample example);
+
+    Demo selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByExample(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByPrimaryKeySelective(Demo record);
+
+    int updateByPrimaryKey(Demo record);
+}
