d5209386eb73ff2fcee0c89b8038e8673e33185b
xfggh <942590550@qq.com>
2021-03-26 11:06:59 +0800

    20210326-跨域配置

---
+++ src/main/java/com/xfggh/blog/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.xfggh.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry){ // 配置跨域
+        registry.addMapping("/**").allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
