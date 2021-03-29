770a809b0a793dfe51d6776766522e0431bad43f
xfggh <942590550@qq.com>
2021-03-29 09:13:30 +0800

    20210329 - 显示电子书列表

--- web/src/views/Home/Home.vue
+++ web/src/views/Home/Home.vue
@@ -20,7 +20,38 @@
         </div>
 
         <div class="home-right">
-            {{ ebooks }}
+            <a-list :grid="{column: 2, gutter: 0}" item-layout="vertical" size="small" :pagination="false" :data-source="ebooks">
+                <template #footer>
+                    <div>
+                        <b>ant design vue</b>
+                        footer part
+                    </div>
+                </template>
+                <template #renderItem="{ item }">
+                    <a-list-item key="item.name">
+                        <template #actions>
+                            <span v-for="{ type, text } in actions" :key="type">
+                                <component v-bind:is="type" style="margin-right: 8px" />
+                                {{ text }}
+                            </span>
+                        </template>
+                        <!-- <template #extra>
+                        <img
+                            width="272"
+                            alt="logo"
+                            src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                        />
+                        </template> -->
+                        <a-list-item-meta :description="item.description">
+                            <template #title>
+                                <a :href="item.href">{{ item.name }}</a>
+                            </template>
+                            <template #avatar><a-avatar :src="item.cover" /></template>
+                        </a-list-item-meta>
+                        {{ item.content }}
+                    </a-list-item>
+                </template>
+            </a-list>
         </div>
     </div>
 </template>
@@ -48,9 +79,16 @@ export default defineComponent({
             });
         });
 
+        const actions: Record<string, string>[] = [
+            { type: 'StarOutlined', text: '156' },
+            { type: 'LikeOutlined', text: '156' },
+            { type: 'MessageOutlined', text: '2' },
+        ];
+
         return{
             ebooks,
-            // ebooks2: toRef(ebooks2, 'books')
+            // ebooks2: toRef(ebooks2, 'books'),
+            actions
         }
     }
 })
@@ -64,8 +102,16 @@ export default defineComponent({
     display: flex;
     
     .home-left{
-        width: 20%;
+        width: 250px;
+        margin: 0 16px 16px 0;
         border-bottom: 1px solid #F0F0F0;
+
+        .ant-menu{
+            width: 200px;
+        }
+
+        // box-shadow: 2px 4px 6px 0 rgba(0,0,0,0.04);
+        // border-radius: 2px;
     }
 }
 </style>
