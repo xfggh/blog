d1f7aa13b97bc8b9d786e9ddd8f31d130de9702a
xfggh <942590550@qq.com>
2021-04-28 08:40:11 +0800

    注释 过滤器和拦截器

--- src/main/java/com/xfggh/blog/config/SpringMvcConfig.java
+++ src/main/java/com/xfggh/blog/config/SpringMvcConfig.java
@@ -1,19 +1,19 @@
-package com.xfggh.blog.config;
-
-import com.xfggh.blog.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-    @Resource
-    LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+// package com.xfggh.blog.config;
+//
+// import com.xfggh.blog.interceptor.LogInterceptor;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+// import javax.annotation.Resource;
+//
+// @Configuration
+// public class SpringMvcConfig implements WebMvcConfigurer {
+//     @Resource
+//     LogInterceptor logInterceptor;
+//
+//     public void addInterceptors(InterceptorRegistry registry) {
+//         registry.addInterceptor(logInterceptor)
+//                 .addPathPatterns("/**").excludePathPatterns("/login");
+//     }
+// }
