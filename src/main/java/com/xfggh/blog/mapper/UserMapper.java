3c37bebde8b9ef50a9744bb45bdbfd8211ed6943
xfggh <942590550@qq.com>
2021-05-25 15:37:48 +0800

    新建用户表

---
+++ src/main/java/com/xfggh/blog/mapper/UserMapper.java
@@ -0,0 +1,36 @@
+package com.xfggh.blog.mapper;
+
+import com.xfggh.blog.entity.User;
+import com.xfggh.blog.entity.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(@Param("host") String host, @Param("user") String user);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExampleWithBLOBs(UserExample example);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(@Param("host") String host, @Param("user") String user);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKeyWithBLOBs(User record);
+
+    int updateByPrimaryKey(User record);
+}
