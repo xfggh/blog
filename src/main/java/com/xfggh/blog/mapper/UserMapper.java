d2e98824e6156b0babf4f4885db4598d5c18b447
xfggh <942590550@qq.com>
2021-05-25 17:21:42 +0800

    解决 mybatis-generator 在不同数据库 存在同名表时 的问题

--- src/main/java/com/xfggh/blog/mapper/UserMapper.java
+++ src/main/java/com/xfggh/blog/mapper/UserMapper.java
@@ -10,27 +10,21 @@ public interface UserMapper {
 
     int deleteByExample(UserExample example);
 
-    int deleteByPrimaryKey(@Param("host") String host, @Param("user") String user);
+    int deleteByPrimaryKey(Long id);
 
     int insert(User record);
 
     int insertSelective(User record);
 
-    List<User> selectByExampleWithBLOBs(UserExample example);
-
     List<User> selectByExample(UserExample example);
 
-    User selectByPrimaryKey(@Param("host") String host, @Param("user") String user);
+    User selectByPrimaryKey(Long id);
 
     int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
-
-    int updateByExampleWithBLOBs(@Param("record") User record, @Param("example") UserExample example);
 
     int updateByExample(@Param("record") User record, @Param("example") UserExample example);
 
     int updateByPrimaryKeySelective(User record);
-
-    int updateByPrimaryKeyWithBLOBs(User record);
 
     int updateByPrimaryKey(User record);
 }
