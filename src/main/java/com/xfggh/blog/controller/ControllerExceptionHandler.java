71cf353a0ffe09d5c61a3f2fb88358325b49bfea
xfggh <942590550@qq.com>
2021-05-26 17:31:54 +0800

    添加 自定义异常

--- src/main/java/com/xfggh/blog/controller/ControllerExceptionHandler.java
+++ src/main/java/com/xfggh/blog/controller/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.xfggh.blog.controller;
 
+import com.xfggh.blog.exception.BusinessException;
 import com.xfggh.blog.resp.CommonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +34,36 @@ public class ControllerExceptionHandler {
         return commonResp;
     }
 
-    
+    /**
+     * 业务异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = BusinessException.class)
+    @ResponseBody
+    public CommonResp validExceptionHandler(BusinessException e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.warn("业务异常：{}", e.getCode().getDesc());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getCode().getDesc());
+        return commonResp;
+    }
+
+    /**
+     * 系统 异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public CommonResp validExceptionHandler(Exception e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("系统异常：", e);
+        commonResp.setSuccess(false);
+        commonResp.setMessage("系统出现异常");
+        return commonResp;
+    }
+
+
 
 }
